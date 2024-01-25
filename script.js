dfb256a22bb5601d90475aad1e9eedb2e17fe1b9
Matt Wyatt <mswyatt.td@gmail.com>
2024-01-24 21:28:49 -0700

    added comments to try and improve readability

--- script.js
+++ script.js
@@ -136,19 +136,21 @@ function avgGrade(ag, submissions) {
 
 }
 
-function addAssignment(ag, submissions) {
-    for (i = 0; i<result.length; i++) {
+function addAssignment(ag, submissions) { 
+    for (i = 0; i<result.length; i++) { //loop through students
       let assignID = 0;
       let assignScore = 0;
       let assignMax = 0
       let assignGrade = 0
       let runningScore = 0
       let runningMax = 0
-       for (j = 0; j<submissions.length; j++) {
-        if(result[i].id == submissions[j].learner_id) {
+       for (j = 0; j<submissions.length; j++) {  //loop through assignments
+        if(result[i].id == submissions[j].learner_id) { //matching student with their assignments
           assignID = submissions[j].assignment_id;
           assignScore = submissions[j].submission.score;
           let dueDate = 0
+
+          //idk if there was a better way to do this but needed the date in this format
           const date = new Date()
           const day = date.getDate()
           const month = date.getMonth()+1
@@ -156,12 +158,12 @@ function addAssignment(ag, submissions) {
           const today = `${year}-`+`${month}-`+`${day}`
 
           for (k = 0; k<ag.assignments.length; k++) {
-            if (ag.assignments[k].id == submissions[j].assignment_id) {
+            if (ag.assignments[k].id == submissions[j].assignment_id) { //matches assignment ID to get the due date and max points
               dueDate = ag.assignments[k].due_at
               const submitDate = submissions[j].submission.submitted_at
-              if (dueDate > today) {continue}
+              if (dueDate > today) {continue} //skips adding to the runningScore if its not due yet
 
-              try {
+              try { //making sure its a number that's not 0
                 100 / ag.assignments[k].points_possible
               } catch (error) {
                 console.error(error)
@@ -169,16 +171,16 @@ function addAssignment(ag, submissions) {
               assignMax = ag.assignments[k].points_possible
               runningMax += assignMax;
 
-              submitDate > dueDate ? assignScore -= (assignMax*.1) : null
+              submitDate > dueDate ? assignScore -= (assignMax*.1) : null //late deduction
               runningScore += assignScore;
             }
           }
-          if (dueDate > today) {continue}
-          assignGrade = Math.round(assignScore / assignMax *1000)/1000
-          result[i][assignID] = assignGrade;
+          if (dueDate > today) {continue} //skips adding the assignment to the student object if the assignment isn't due yet
+          assignGrade = Math.round(assignScore / assignMax *1000)/1000 //converts grade into a percent
+          result[i][assignID] = assignGrade; //adds a new key-value pair with the key being the assignment ID
         }
        }
-       result[i].avg = Math.round(runningScore / runningMax *1000)/1000
+       result[i].avg = Math.round(runningScore / runningMax *1000)/1000 //sets avg rounded to 3 decimals
     }
 }
 
